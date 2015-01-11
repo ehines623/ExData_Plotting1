83d9e1d10a2f2e39697a28645ce6050577612b7c
Elizabeth Hines <elizabeth.hines623@gmail.com>
2015-01-11 17:47:29 -0500

    adding plot 4

--- plot4.R
+++ plot4.R
@@ -11,6 +11,12 @@ DataSub <- filter(fullData, Date == "1/2/2007"| Date == "2/2/2007")
 DataSub$DateTime <- strptime(paste(DataSub$Date,DataSub$Time),
                              format = "%d/%m/%Y %H:%M:%S")
 
+par(mfcol = c(2,2))
+
+plot(DataSub$DateTime, DataSub$Global_active_power, type="l"
+     , xlab = ""
+     , ylab = "Global Active Power (kilowatts)")
+
 plot(DataSub$DateTime, DataSub$Sub_metering_1,type="n", xlab = ""
      , ylab = "Energy sub metering")
 
@@ -21,7 +27,14 @@ legend("topright", c("Sub_metering_1","Sub_metering_2","Sub_metering_3")
        , lty = c(1,1,1), col= c("black","red","blue")
        , text.width= strwidth("Sub_metering_1"))
 
+plot(DataSub$DateTime, DataSub$Voltage, type ="l"
+     , xlab = "datetime", ylab = "Voltage")
 
-dev.copy(png, file = "plot3.png", height=480, width=480, bg = "white"
-         , pointsize = 12)
+plot(DataSub$DateTime, DataSub$Global_reactive_power
+     , type = "l",  xlab = "datetime", ylab="Global_reactive_power")
+
+
+
+dev.copy(png, file = "plot4.png", height=480, width=480, bg = "white")
 dev.off()
+
