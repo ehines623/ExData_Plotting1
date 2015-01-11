48114547febad399779707f02d913bf1604e41f7
Elizabeth Hines <elizabeth.hines623@gmail.com>
2015-01-11 16:32:41 -0500

    First commit plot1

--- plot1.R
+++ plot1.R
@@ -1,13 +1,18 @@
+library(dplyr)
+
 fullData <- read.table("household_power_consumption.txt",header=TRUE, sep =";",
                        na.strings="?",
                        colClasses=c("character","character","numeric","numeric",
                                     "numeric","numeric","numeric","numeric",
                                     "numeric"))
 
+fullData$Date <- as.Date(fullData$Date, "%d/%m/%Y")
+##fullData$Time <- strptime(fullData$Time,"%H:%M:%S")
 
+DataSub <- filter(fullData, Date == "2007-2-1"| Date == "2007-2-2")
 
-hist(fullData$Global_active_power, col = "red", main = "Global Active Power", xlab= "Global Active Power (kilowatts)",
+hist(DataSub$Global_active_power, col = "red", main = "Global Active Power", xlab= "Global Active Power (kilowatts)",
 ,ylab="Frequency")
 
-dev.copy(png, file = "plot1.png")
+dev.copy(png, file = "plot1.png", height=480, width=480, bg = "white")
 dev.off()
